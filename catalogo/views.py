9aa26e67608d03d1a8aa81bed095844cd387f958
LynxLawliet <pedro.caro.villarroel06@gmail.com>
2026-09-06 16:09:33 -0300

    etapa-2-interfaz

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+import json
+import os
+from django.conf import settings
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Vista del catálogo funcionando")
+
+# Función auxiliar para leer el JSON
+def cargar_datos():
+    ruta = os.path.join(settings.BASE_DIR, 'catalogo', 'data', 'productos.json')
+    with open(ruta, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def lista(request):
+    productos = cargar_datos()
+    return render(request, 'catalogo/lista.html', {'productos': productos})
+
+def detalle(request, producto_id):
+    productos = cargar_datos()
+    # Buscar el producto por id
+    producto = next((p for p in productos if p['id'] == producto_id), None)
+    
+    if not producto:
+        raise Http404("Producto no encontrado en la ferretería") # Manejo de caso inexistente
+        
+    return render(request, 'catalogo/detalle.html', {'producto': producto})
+
